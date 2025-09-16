b849dd6f16e7d91eb7dd7aa1d9f1965befa62471
Norman Johann Zachary O. Fernandez <normanjohann.fernandez.cics@ust.edu.ph>
2025-09-16 11:44:38 +0800

    ipInfo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 function Card({ title, ipInfo }) {
-  const { ip, version, country, country_code, region, city, org, asn, latitude, longitude } = ipInfo;
-  return (
+  
+return (
     <div className="p-6 outline-1 outline-gray-200 rounded-lg hover:shadow-md transition-shadow duration-300 mb-4">
       <div className="font-bold text-xl mb-2">{title}</div>
       <ul className="space-y-2 text-gray-700 text-left">
-          <li><strong>IP:</strong> {ip}</li>
-          <li><strong>Version:</strong> {version}</li>
-          <li><strong>Country:</strong> {country} ({country_code})</li>
-          <li><strong>Region:</strong> {region}</li>
-          <li><strong>City:</strong> {city}</li>
-          <li><strong>ISP:</strong> {org}</li>
-          <li><strong>ASN:</strong> {asn}</li>
-          <li><strong>Coordinates:</strong> {latitude}, {longitude}</li>
+          <li><strong>IP:</strong> {ipInfo.ip}</li>
+          <li><strong>Version:</strong> {ipInfo.version}</li>
+          <li><strong>Country:</strong> {ipInfo.country} ({ipInfo.country_code})</li>
+          <li><strong>Region:</strong> {ipInfo.region}</li>
+          <li><strong>City:</strong> {ipInfo.city}</li>
+          <li><strong>ISP:</strong> {ipInfo.org}</li>
+          <li><strong>ASN:</strong> {ipInfo.asn}</li>
+          <li><strong>Coordinates:</strong> {ipInfo.latitude}, {ipInfo.longitude}</li>
         </ul>
     </div>
   );
