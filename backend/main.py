cf599ebeaf56c63f33d7f49754e85132304b3503
Norman Johann Zachary O. Fernandez <normanjohann.fernandez.cics@ust.edu.ph>
2025-09-24 15:49:32 +0800

    Validation: fix state handling in App.jsx

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -6,33 +6,42 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # adjust if needed
+    allow_origins=["http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-BASE_URL = "https://ipapi.co"
+API_KEY = "cbae7561b0f9d110330f373da8ba45ef" 
+BASE_URL = "http://api.ipstack.com"
 
 @app.get("/ipinfo/{ip}")
 def get_ip_info(ip: str):
-    url = f"{BASE_URL}/{ip}/json/"
+    url = f"{BASE_URL}/{ip}?access_key={API_KEY}"
     response = requests.get(url)
 
     if response.status_code != 200:
-        return {"error": "Failed to fetch data from ipapi.co"}
+        raise HTTPException(status_code=500, detail="Failed to fetch data from Ipstack")
 
     data = response.json()
+
+    if (
+        "error" in data
+        or not data.get("ip")
+        or data.get("country_name") is None
+        or (data.get("latitude") == 0 and data.get("longitude") == 0)
+    ):
+        raise HTTPException(status_code=400, detail=f'IP address "{ip}" is invalid')
 
     return {
         "ip": data.get("ip"),
         "version": "IPv4" if "." in data.get("ip", "") else "IPv6",
         "country": data.get("country_name"),
-        "country_code": data.get("country"),
-        "region": data.get("region"),
+        "country_code": data.get("country_code"),
+        "region": data.get("region_name"),
         "city": data.get("city"),
-        "org": data.get("org", "Unknown"),
-        "asn": data.get("asn", "Unknown"),
+        "org": data.get("connection", {}).get("isp", "Unknown"),
+        "asn": data.get("connection", {}).get("asn", "Unknown"),
         "latitude": data.get("latitude"),
         "longitude": data.get("longitude"),
     }
