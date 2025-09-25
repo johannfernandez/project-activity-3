a6f1a148058a4565f18c6be1ef3e66481fccbc3b
Norman Johann Zachary O. Fernandez <normanjohann.fernandez.cics@ust.edu.ph>
2025-09-25 14:55:58 +0800

    fixed the issue with the search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 function Search({ onSearch }) {
   const [input, setInput] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (input.trim()) {
       onSearch(input.trim());
     }
   };
 
   return (
-    <div className="flex p-6 outline-1 outline-gray-200 rounded-lg transition-shadow duration-300 mb-4">
+    <form
+      onSubmit={handleSubmit}
+      className="flex p-6 outline-1 outline-gray-200 rounded-lg transition-shadow duration-300 mb-4"
+    >
       <input
         type="text"
         placeholder="Enter IP address"
@@ -19,12 +23,12 @@ function Search({ onSearch }) {
         className="p-4 bg-gray-100 outline-1 outline-gray-200 rounded-lg basis-5/6 me-3"
       />
       <button
-        onClick={handleSubmit}
+        type="submit"
         className="p-4 bg-orange-400 rounded-lg basis-1/6 hover:shadow-md hover:shadow-orange-300 transition-shadow duration-300"
       >
         <p className="text-white text-center font-bold">Search IP Address</p>
       </button>
-    </div>
+    </form>
   );
 }
 
