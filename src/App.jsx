14c4aeb2c515a717585df94f32968f1e53beb9f9
Norman Johann Zachary O. Fernandez <normanjohann.fernandez.cics@ust.edu.ph>
2025-11-17 15:59:37 +0800

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
         &copy; {new Date().getFullYear()} Johann Fernandez. All rights reserved.
       </p>
     </div>
-    //THIS IS A TEST COMMENTt
+    //This is the main application component that manages state and handles IP address searches.
   );
 }
 
